620b0763e9f4d9f19127c8901fbeafe8676ef310
Balogunmba22 <balogunmba22@gmail.com>
2023-03-15 22:31:21 +0100

    finished styling the homepage

--- src/utilities/styles.js
+++ src/utilities/styles.js
@@ -1,6 +1,7 @@
 const styles = {
-  paddingX: "sm:px-16 px-2",
+  paddingX: "sm:px-16 px-6",
   paddingY: "sm:py-16 py-6",
+  paddingZ: "sm:px-16 px-0",
   padding: "sm:px-16 px-6 sm:py-16 py-10",
 
   heroHeadText:
